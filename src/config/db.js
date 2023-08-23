ff8d587b53f31991f6728e6105c75ab848e8e853
Onlygideon <stephengideon22@gmail.com>
2023-08-23 14:07:19 +0100

    edit: db config url

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,13 @@ const mongoLocal = "mongodb://127.0.0.1:27017/tobams_group";
 
 const connectMongodb = async () => {
   try {
-    await mongoose.connect(mongoLocal);
-    console.log("MongoDB local connection established");
+    if (process.env.DB_URL) {
+      await mongoose.connect(`${process.env.DB_URL}`);
+      console.log("MongoDB remote connection established");
+    } else {
+      await mongoose.connect(mongoLocal);
+      console.log("MongoDB local connection established");
+    }
   } catch (err) {
     console.log("MongoDB connection - retrying", err);
 
